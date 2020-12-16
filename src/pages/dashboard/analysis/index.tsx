cae7b71820ed6ce1a47cfcfb5d9227c35843927a
“李鹏29306” <654488010@qq.com>
2020-12-16 20:29:10 +0800

    “路由删除”

--- src/pages/dashboard/analysis/index.tsx
+++ src/pages/dashboard/analysis/index.tsx
@@ -1,45 +1,11 @@
-// import { EllipsisOutlined } from '@ant-design/icons';
-import { Col, Dropdown, Menu, Row } from 'antd';
-import React, { Component, Suspense } from 'react';
-import { GridContent } from '@ant-design/pro-layout';
-import { RadioChangeEvent } from 'antd/es/radio';
-import { RangePickerProps } from 'antd/es/date-picker/generatePicker';
-import moment from 'moment';
-import { connect, Dispatch } from 'umi';
 
-import PageLoading from './components/PageLoading';
-import { getTimeDistance } from './utils/utils';
-
-const IntroduceRow = React.lazy(() => import('./components/IntroduceRow'));
-const SalesCard = React.lazy(() => import('./components/SalesCard'));
-const TopSearch = React.lazy(() => import('./components/TopSearch'));
-const ProportionSales = React.lazy(() => import('./components/ProportionSales'));
-const OfflineData = React.lazy(() => import('./components/OfflineData'));
-
-type RangePickerValue = RangePickerProps<moment.Moment>['value'];
-
-
-
+import React, { Component } from 'react';
 class Analysis extends Component {
   render() {
     return (
-      <div>22</div>
+      <div>用户管理页面</div>
 
     );
   }
 }
-
-export default connect(
-  ({
-    dashboardAndanalysis,
-    loading,
-  }: {
-    dashboardAndanalysis: any;
-    loading: {
-      effects: { [key: string]: boolean };
-    };
-  }) => ({
-    dashboardAndanalysis,
-    loading: loading.effects['dashboardAndanalysis/fetch'],
-  }),
-)(Analysis);
+export default Analysis
